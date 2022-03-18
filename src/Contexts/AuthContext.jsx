b4415c0cc5bd7c6c3c4b71bbcaeead9e8ab64ac0
Vinícius Quadrado <68359941+viniciuSquare@users.noreply.github.com>
2022-03-18 14:43:39 -0300

    Organize contexts - auth and session. Improved mains file routing.

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,15 +1,19 @@
 import {createContext, useState} from 'react'
+import { useEffect } from 'react/cjs/react.development';
 
 export const AuthContext = createContext({});
 
 export function AuthContextProvider(props) {
-  let userData = (localStorage.getItem("user")) || "";
+  let userData = JSON.parse(localStorage.getItem("user")) || {};
 
-  const [user, setUser] = useState({
-    logged: false,
-    userData
-  });
-  
+  const [user, setUser] = useState(userData);
+
+  useEffect(()=>{
+    const localStorageUserData = JSON.stringify(user);
+    localStorage.setItem('user', localStorageUserData);
+
+  }, [user])
+    
   return(
     <AuthContext.Provider value={{user, setUser}}>
       {props.children}
